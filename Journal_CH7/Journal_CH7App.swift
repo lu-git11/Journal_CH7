3f6d66657611ad9d637a549f080f86521008609a
Jeffrey Lullen <lullenj@gmail.com>
2026-04-28 08:58:46 +0800

    Initial Commit

---
+++ Journal_CH7/Journal_CH7App.swift
@@ -0,0 +1,17 @@
+//
+//  Journal_CH7App.swift
+//  Journal_CH7
+//
+//  Created by jeffrey lullen on 4/28/26.
+//
+
+import SwiftUI
+
+@main
+struct Journal_CH7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
